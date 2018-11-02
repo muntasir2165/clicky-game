580d6632e820043d1fe5e3c24bf03e79ac45d411
muntasir2165 <muntasir2165@hotmail.com>
2018-11-02 00:17:27 -0400

    Updates

---
+++ precache-manifest.b466eb0586679605905a6173a3d539d6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "68e23a37d7cabd59c0e881ac61994e32",
+    "url": "/clicky-game/static/media/kearney.68e23a37.png"
+  },
+  {
+    "revision": "181133b8a2345d220c70",
+    "url": "/clicky-game/static/css/main.82f293eb.chunk.css"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/clicky-game/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "0c919e8958723f9616879f933f6809d8",
+    "url": "/clicky-game/static/media/banner-background.0c919e89.jpg"
+  },
+  {
+    "revision": "f86dad364d1526ebe84ff5922a04eef0",
+    "url": "/clicky-game/static/media/abe.f86dad36.jpg"
+  },
+  {
+    "revision": "0dac2be7692ed63e3b685bc095a1a3c6",
+    "url": "/clicky-game/static/media/apu.0dac2be7.jpg"
+  },
+  {
+    "revision": "ec24508d0af889a3f6bad0c2343f570e",
+    "url": "/clicky-game/static/media/banner-background-1.ec24508d.jpg"
+  },
+  {
+    "revision": "1f68b8001e6843bbe4db3dcc4d540205",
+    "url": "/clicky-game/static/media/banner-background-2.1f68b800.jpg"
+  },
+  {
+    "revision": "4d832e9b5d78f9f006b1d84e9d47daa1",
+    "url": "/clicky-game/static/media/barney.4d832e9b.jpg"
+  },
+  {
+    "revision": "1a693173e1e39272f23561613c98fa78",
+    "url": "/clicky-game/static/media/bart-2.1a693173.jpg"
+  },
+  {
+    "revision": "9a4532fa1cf0b3c051899c932d9fe38e",
+    "url": "/clicky-game/static/media/bart.9a4532fa.jpg"
+  },
+  {
+    "revision": "a4b3bb76f83301224018427806d925da",
+    "url": "/clicky-game/static/media/edna.a4b3bb76.jpg"
+  },
+  {
+    "revision": "8dbc6cef20bc002a8d5170cabb86d59b",
+    "url": "/clicky-game/static/media/homer.8dbc6cef.png"
+  },
+  {
+    "revision": "181133b8a2345d220c70",
+    "url": "/clicky-game/static/js/main.181133b8.chunk.js"
+  },
+  {
+    "revision": "6311b16cb64708a9a8e9717a005f9cd2",
+    "url": "/clicky-game/static/media/lisa.6311b16c.jpg"
+  },
+  {
+    "revision": "3696b34b8a3fed2cf5f0a620ad8090a2",
+    "url": "/clicky-game/static/media/maggie.3696b34b.jpg"
+  },
+  {
+    "revision": "688c960f4530536ef5c05060e03ace24",
+    "url": "/clicky-game/static/media/marge.688c960f.jpg"
+  },
+  {
+    "revision": "2fbcafd73602df52aca0be2f59a26c5c",
+    "url": "/clicky-game/static/media/milhouse.2fbcafd7.png"
+  },
+  {
+    "revision": "465cfc146b5083c278d523452ae9eb33",
+    "url": "/clicky-game/static/media/moe.465cfc14.jpg"
+  },
+  {
+    "revision": "8cadf3a0f288c13fae811bf25614ca0f",
+    "url": "/clicky-game/static/media/mr-burns.8cadf3a0.jpg"
+  },
+  {
+    "revision": "73537af1ddc3ee26c165673778a77fb3",
+    "url": "/clicky-game/static/media/ned.73537af1.jpg"
+  },
+  {
+    "revision": "1dee920b468528de8a897d5c5f141014",
+    "url": "/clicky-game/static/media/ralph.1dee920b.png"
+  },
+  {
+    "revision": "f06f86794369c491cb546afcd930358b",
+    "url": "/clicky-game/static/media/skinner.f06f8679.jpg"
+  },
+  {
+    "revision": "0f2d286c6eaee91790fde2a7b3dd4e6c",
+    "url": "/clicky-game/static/media/tony.0f2d286c.jpg"
+  },
+  {
+    "revision": "1bfbdf8a36d776c6326a62ba87d12eb1",
+    "url": "/clicky-game/static/media/wiggum.1bfbdf8a.jpg"
+  },
+  {
+    "revision": "181efdf989eda20ef6a2465874636758",
+    "url": "/clicky-game/static/media/willie.181efdf9.jpg"
+  },
+  {
+    "revision": "43489f7aeb3955c4dc6ef3095dbfa303",
+    "url": "/clicky-game/index.html"
+  }
+];
